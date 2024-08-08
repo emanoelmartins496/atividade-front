f50dd2f6bb1275a6f25983fbf59b8a4998c3fdf3
Andreina Lima <andreinaadelima14@gmail.com>
2024-08-08 15:11:58 -0300

    Feito editar aluno e curso

--- atividade-front/src/pages/EditarAluno.jsx
+++ atividade-front/src/pages/EditarAluno.jsx
@@ -1,5 +1,177 @@
+import { Button } from "react-bootstrap"
+import { useForm } from "react-hook-form"
+import { useNavigate, useParams } from "react-router-dom"
+import toast from "react-hot-toast"
+import { useEffect } from "react"
+import { getAluno, updateAluno } from "../api/clientes"
+
 function EditarAluno() {
-  return <main></main>
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm()
+
+  const navigate = useNavigate()
+  const { id } = useParams()
+
+  function atualizarAluno(data) {
+    updateAluno(id, data)
+      .then((resposta) => {
+        toast.success(resposta.message)
+        navigate("/alunos")
+      })
+      .catch((err) => {
+        toast.error(err.response.data.message)
+      })
+  }
+
+  function carregarAluno() {
+    getAluno(id)
+      .then((dados) => {
+        reset(dados)
+      })
+      .catch((err) => {
+        navigate("/alunos")
+      })
+  }
+
+  useEffect(() => {
+    carregarAluno()
+  }, [])
+
+  return (
+    <main className="mt-4 container">
+      <h1>Editar Aluno</h1>
+      <hr />
+      <form onSubmit={handleSubmit(atualizarAluno)}>
+        <div>
+          <label htmlFor="matricula">Matrícula</label>
+          <input
+            type="text"
+            id="matricula"
+            className="form-control"
+            {...register("matricula", { required: true, maxLength: 50 })}
+          />
+          {errors.matricula && (
+            <small className="text-danger">A matrícula é inválida!</small>
+          )}
+        </div>
+        <div>
+          <label htmlFor="nome">Nome</label>
+          <input
+            type="text"
+            id="nome"
+            className="form-control"
+            {...register("nome", { required: true, maxLength: 200 })}
+          />
+          {errors.nome && (
+            <small className="text-danger">O nome é inválido!</small>
+          )}
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            className="form-control"
+            {...register("email", { required: true, maxLength: 200 })}
+          />
+          {errors.email && (
+            <small className="text-danger">O email é inválido!</small>
+          )}
+        </div>
+        <div>
+          <label htmlFor="telefone">Telefone</label>
+          <input
+            type="tel"
+            id="telefone"
+            className="form-control"
+            {...register("telefone", { required: true, maxLength: 15 })}
+          />
+          {errors.telefone && (
+            <small className="text-danger">O telefone é inválido!</small>
+          )}
+        </div>
+        <div>
+          <label htmlFor="senha">Senha</label>
+          <input
+            type="password"
+            id="senha"
+            className="form-control"
+            {...register("senha", { required: true, minLength: 6 })}
+          />
+          {errors.senha && (
+            <small className="text-danger">A senha é inválida!</small>
+          )}
+        </div>
+        <div>
+          <label htmlFor="cidade">Cidade</label>
+          <input
+            type="text"
+            id="cidade"
+            className="form-control"
+            {...register("endereco.cidade", { required: true, maxLength: 200 })}
+          />
+          {errors.endereco?.cidade && (
+            <small className="text-danger">A cidade é inválida!</small>
+          )}
+        </div>
+        <div>
+          <label htmlFor="uf">UF</label>
+          <input
+            type="text"
+            id="uf"
+            className="form-control"
+            {...register("endereco.uf", { required: true, maxLength: 2 })}
+          />
+          {errors.endereco?.uf && (
+            <small className="text-danger">A UF é inválida!</small>
+          )}
+        </div>
+        <div>
+          <label htmlFor="cep">CEP</label>
+          <input
+            type="text"
+            id="cep"
+            className="form-control"
+            {...register("endereco.cep", { required: true, maxLength: 8 })}
+          />
+          {errors.endereco?.cep && (
+            <small className="text-danger">O CEP é inválido!</small>
+          )}
+        </div>
+        <div>
+          <label htmlFor="rua">Rua</label>
+          <input
+            type="text"
+            id="rua"
+            className="form-control"
+            {...register("endereco.rua", { required: true })}
+          />
+          {errors.endereco?.rua && (
+            <small className="text-danger">A rua é inválida!</small>
+          )}
+        </div>
+        <div>
+          <label htmlFor="numero">Número</label>
+          <input
+            type="text"
+            id="numero"
+            className="form-control"
+            {...register("endereco.numero", { required: true })}
+          />
+          {errors.endereco?.numero && (
+            <small className="text-danger">O número é inválido!</small>
+          )}
+        </div>
+        <Button className="mt-3" type="submit">
+          Atualizar
+        </Button>
+      </form>
+    </main>
+  )
 }
 
 export default EditarAluno
--- atividade-front/src/pages/EditarCurso.jsx
+++ atividade-front/src/pages/EditarCurso.jsx
@@ -1,5 +1,81 @@
+import { Button } from "react-bootstrap"
+import { useForm } from "react-hook-form"
+import { useNavigate, useParams } from "react-router-dom"
+import toast from "react-hot-toast"
+import { useEffect } from "react"
+import { getCurso, updateCurso } from "../api/clientes"
+
 function EditarCurso() {
-  return <main></main>
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm()
+
+  const navigate = useNavigate()
+  const { id } = useParams()
+
+  function atualizarCurso(data) {
+    updateCurso(id, data)
+      .then((resposta) => {
+        toast.success(resposta.message)
+        navigate("/cursos")
+      })
+      .catch((err) => {
+        toast.error(err.response.data.message)
+      })
+  }
+
+  function carregarCurso() {
+    getCurso(id)
+      .then((dados) => {
+        reset(dados)
+      })
+      .catch((err) => {
+        navigate("/cursos")
+      })
+  }
+
+  useEffect(() => {
+    carregarCurso()
+  }, [])
+
+  return (
+    <main className="mt-4 container">
+      <h1>Editar Curso</h1>
+      <hr />
+      <form onSubmit={handleSubmit(atualizarCurso)}>
+        <div>
+          <label htmlFor="tiposDeCursos">Tipo de Curso</label>
+          <input
+            type="text"
+            id="tiposDeCursos"
+            className="form-control"
+            {...register("tiposDeCursos", { required: true, maxLength: 200 })}
+          />
+          {errors.tiposDeCursos && (
+            <small className="text-danger">O tipo de curso é inválido!</small>
+          )}
+        </div>
+        <div>
+          <label htmlFor="modulo">Módulo</label>
+          <input
+            type="number"
+            id="modulo"
+            className="form-control"
+            {...register("modulo", { required: true, min: 1 })}
+          />
+          {errors.modulo && (
+            <small className="text-danger">O módulo é inválido!</small>
+          )}
+        </div>
+        <Button className="mt-3" type="submit">
+          Atualizar
+        </Button>
+      </form>
+    </main>
+  )
 }
 
 export default EditarCurso
