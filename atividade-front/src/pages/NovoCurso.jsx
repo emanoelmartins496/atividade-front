983248daf6969e0d78fdb4ed2cdbea34b85ed7ae
Emanoel Martins <139373825+emanoelmartins496@users.noreply.github.com>
2024-08-08 17:25:14 -0300

    Inclusão do arquivo Novos Cursos

--- atividade-front/src/pages/NovoCurso.jsx
+++ atividade-front/src/pages/NovoCurso.jsx
@@ -1,5 +1,68 @@
+import { Button } from "react-bootstrap"
+import { useForm } from "react-hook-form"
+import { useNavigate, useParams } from "react-router-dom"
+import toast from "react-hot-toast"
+import { addCurso } from "../api/cursos"
+
 function NovoCurso() {
-  return <main></main>
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const navigate = useNavigate();
+
+  function criarCurso(data) {
+    addCurso(data)
+      .then((resposta) => {
+        toast.success(resposta.message);
+        navigate("/cursos");
+      })
+      .catch((err) => {
+        toast.error(err.response.data.message);
+      });
+  }
+
+  return (
+    <main className="mt-4 container">
+      <h1>Novo Curso</h1>
+      <hr />
+      <form onSubmit={handleSubmit(criarCurso)}>
+        <div>
+          <label htmlFor="tiposDeCursos">Tipo de Curso</label>
+          <input
+            type="text"
+            id="tiposDeCursos"
+            className="form-control"
+            {...register("tiposDeCursos", { required: true, maxLength: 200 })}
+          />
+          {errors.tiposDeCursos && (
+            <small className="text-danger">O tipo de curso é inválido!</small>
+          )}
+        </div>
+        <div>
+          <label htmlFor="modulo">Módulo</label>
+          <input
+            type="number"
+            id="modulo"
+            className="form-control"
+            min="1"
+            {...register("modulo", { 
+              required: "O módulo é obrigatório", 
+              min: { value: 1, message: "O módulo deve ser pelo menos 1" } 
+            })}
+          />
+          {errors.modulo && (
+            <small className="text-danger">O módulo é inválido!</small>
+          )}
+        </div>
+        <Button className="mt-3" type="submit">
+          Cadastrar
+        </Button>
+      </form>
+    </main>
+  );
 }
 
-export default NovoCurso
+export default NovoCurso;
