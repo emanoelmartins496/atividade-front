c1c21072d1ac40f75932652e640ac72f691b49e7
Emanoel Martins <139373825+emanoelmartins496@users.noreply.github.com>
2024-08-08 16:32:12 -0300

    Inclusão Navbar

--- atividade-front/src/components/Header.jsx
+++ atividade-front/src/components/Header.jsx
@@ -1,20 +1,37 @@
-import logo from "../../public/img/logo.png"
-import { Link } from "react-router-dom"
+import React from "react";
+import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
 
 function Header() {
   return (
-    <header className="header w-100 px-3 py-2">
-      <nav className="container d-flex justify-content-between align-items-center">
-        <Link to="/">
-          <img src={logo} alt="Devnation Academy logo" />
-        </Link>
-        <div className="d-flex gap-5">
-          <Link to="/alunos">Alunos</Link>
-          <Link to="/cursos">Cursos</Link>
-        </div>
-      </nav>
-    </header>
-  )
+    <Navbar expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">
+          DevNation <span className="font-weight-bold" style={{ color: "#0091FF" }}>Academy</span>
+        </Navbar.Brand>
+        
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav style={{ marginLeft: 'auto' }} className="ml-auto">
+            <Nav.Link href="#cursos" style={{ color: "#0091FF" }}>Cursos</Nav.Link>
+            <Nav.Link href="#sobre" style={{ color: "#0091FF" }}>Sobre</Nav.Link>
+            <Nav.Link href="#PolíticaDePrivacidade" style={{ color: "#0091FF" }}>Política de privacidade</Nav.Link>
+            <NavDropdown title="Portal do aluno" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#login" style={{ color: "#0091FF", fontWeight: 'bold' }}>
+                Login
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#cadastro" style={{ color: "#0091FF", fontWeight: 'bold' }}>
+                Cadastro
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#contato" style={{ color: "#0091FF" }}>Contato</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
-export default Header
+export default Header;
+
+
--- atividade-front/src/pages/Home.jsx
+++ atividade-front/src/pages/Home.jsx
@@ -1,6 +1,8 @@
 function Home() {
   return(
-    <main></main>
+    <main>
+      
+    </main>
   )
 }
 
